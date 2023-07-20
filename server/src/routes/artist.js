f83230e2b076830715c174511067b3b401f0b7e2
Sungwoon95 <lad0305@naver.com>
2023-07-20 17:49:04 +0900

    []

--- server/src/routes/artist.js
+++ server/src/routes/artist.js
@@ -1,25 +1,25 @@
-import {readJSON} from '../jsonController.js'
+import { readJSON } from '../jsonController.js'
 
-const artistRoute=[
+const artistRoute = [
   {
-    method:'get',
-    route:'/artist',
-    handler:(req,res)=>{
+    method: 'get',
+    route: '/artist',
+    handler: (req, res) => {
       const artist = readJSON('artist')
       res.send(artist)
     }
-  },{
-    method:'get',
-    route:'/artist/:name',
-    handler:({params:{name}},res)=>{
+  }, {
+    method: 'get',
+    route: '/artist/:name',
+    handler: ({ params: { name } }, res) => {
       try {
         const artist = readJSON('artist')
         const album = readJSON('album')
         const target = artist[name]
-        const targetAlbum = album.find((i)=> i.artistname===name)
-        console.log({targetAlbum, target})
-        if(! target) throw Error('해당 아티스트가 없습니다.')
-        res.send({target,targetAlbum})        
+        const targetAlbum = album.filter((i) => i.artistname === name)
+        console.log({ targetAlbum, target })
+        if (!target) throw Error('해당 아티스트가 없습니다.')
+        res.send({ target, targetAlbum })
       } catch (error) {
         console.error(error)
       }
