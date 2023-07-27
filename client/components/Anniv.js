c56e65a3b46f1594805c77e5d38871427bcf26b3
Sungwoon95 <lad0305@naver.com>
2023-07-27 10:00:20 +0900

    [feat]n년 전 오늘 데뷔한 아티스트 표시

--- client/components/Anniv.js
+++ client/components/Anniv.js
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import { useState } from "react";
 import { calcDate } from "../utils/calcDate";
 
 const Anniv = (
@@ -12,41 +11,42 @@ const Anniv = (
   console.log(rest)
   return (
     <section className='anniv_card_wrap'>
-      <div className={'anniv_artist_shadow'}>
-        <div className={'anniv_artist_main'}>
-          <div className={'anniv_main_img'}>
-            <img src={`http://localhost:8000/${thumbnail}`} />
-          </div>
-          <div className={'anniv_card_item'}>
-            <div className={'anniv_card_img_wrap'}>
-              <div className={'anniv_card_img'}>
-                <img src={`http://localhost:8000/${imgurl}`} />
+      <Link href={`/artist/${name.replace(' ', '')}`}>
+        <div className={'anniv_artist_shadow'}>
+          <div className={'anniv_artist_main'}>
+            <div className={'anniv_main_img'}>
+              <img src={`http://localhost:8000/${thumbnail}`} />
+            </div>
+            <div className={'anniv_card_item'}>
+              <div className={'anniv_card_img_wrap'}>
+                <div className={'anniv_card_img'}>
+                  <img src={`http://localhost:8000/${imgurl}`} />
+                </div>
+              </div>
+              <div className={'anniv_card_info'}>
+                <div className={'flex'}>
+                  <span className={'album_type'}>
+                    {albumtype}
+                  </span>
+                  <p>
+                    {albumtitle}
+                  </p>
+                </div>
+                <time dateTime={calcDate(date).dateTime}>
+                  {calcDate(date, '.').type}
+                </time>
               </div>
             </div>
-            <div className={'anniv_card_info'}>
-              <div className={'flex'}>
-                <span className={'album_type'}>
-                  {albumtype}
-                </span>
-                <p>
-                  {albumtitle}
-                </p>
-              </div>
-              <time dateTime={calcDate(date).dateTime}>
-                {calcDate(date, '.').type}
-              </time>
+          </div>
+          <div className={'anniv_card_desc'}>
+            <h3>
+              {year}년 전 {name_kor}
+            </h3>
+            <div>
+              <span style={{ color: '#458aff' }}>데뷔 앨범</span>
+              <p style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>{albumtitle}</p>
             </div>
-          </div>
-        </div>
-        <div className={'anniv_card_desc'}>
-          <h3>
-            {year}년 전 {name_kor}
-          </h3>
-          <div>
-            <span style={{ color: '#458aff' }}>데뷔 앨범</span>
-            <p style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>{albumtitle}</p>
-          </div>
-          {/* <div className={'flex'}>
+            {/* <div className={'flex'}>
             <p>수록곡 수 : </p>
             <p>{song.length}</p>
           </div>
@@ -54,8 +54,9 @@ const Anniv = (
             <p>수록곡 목록 : </p>
             <p>{song.map(i => i)}</p>
           </div> */}
+          </div>
         </div>
-      </div>
+      </Link>
     </section>
 
   )
